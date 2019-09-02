8c94229acea9029c60b5188ccda7340579cd4cff
Breno Azevedo <maddox.br@gmail.com>
2019-09-02 18:03:27 -0300

    Finished Translation / Rotation

--- Assets/Scripts/PointAtMouse.cs
+++ Assets/Scripts/PointAtMouse.cs
@@ -1,28 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Net.Security;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PointAtMouse : MonoBehaviour
 {
-    public float TurnSpeed = 20f;
+    private bool _cameraIsDefined;
+    public float MoveSpeed = 50f;
+    public float TurnSpeed = 300f;
+    public float SmoothFactor = 0.25f;
+    
+    enum axis { Horizontal, Vertical }
 
-    // Update is called once per frame
-    void Update()
-    {
-        var clickWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    private void Awake() {
+        _cameraIsDefined = (Camera.main != null);
+    }
+
+    void Update() {
+        if (! _cameraIsDefined)
+            return;
         
-        var direction =  clickWorldPos - transform.position; // target.position - transform.position;
-        
+        var pointedWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var dt = Time.deltaTime;
+        SmoothLookAtPos(transform, pointedWorldPos, TurnSpeed, SmoothFactor, dt);
+
+        var translation = new Vector2(Input.GetAxis(axis.Horizontal + ""),
+                                      Input.GetAxis(axis.Vertical + ""));
+        SmoothTranslate(transform, translation, MoveSpeed, SmoothFactor, dt);
+    }
+
+    public void SmoothTranslate(Transform transf, Vector2 translation, float moveSpeed, float t, float dt) {
+        if (Mathf.Approximately(translation.x, 0) && Mathf.Approximately(translation.y, 0))
+            return;
+        var smoothTranslation = Vector2.Lerp(Vector2.zero, translation, t);
+        transf.Translate(smoothTranslation * dt * moveSpeed, Space.World);
+    }
+
+    public void SmoothLookAtPos(Transform transf, Vector3 targetPos, float turnSpeed, float t, float dt) {
+        var direction = targetPos - transf.position; // target pos - origin pos;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        //Quaternion.AngleAxis(angle, Vector3.forward);
-        var rotation = Quaternion.Euler(0f, 0f, angle);
-        
-        //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, TurnSpeed * Time.deltaTime);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 10f);
+        // Below is the same as: Quaternion.AngleAxis(angle, Vector3.forward);
+        var targetRotation = Quaternion.Euler(0f, 0f, angle);
 
-        //var newTargetPos = new Vector2(clickWorldPos.x, clickWorldPos.y);
-        if (Input.GetMouseButton(1))
-            transform.position = Vector2.MoveTowards(transform.position, clickWorldPos, 0.1f);
+        var smoothRotation = Quaternion.Slerp(transf.rotation, targetRotation, t);
+
+        //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, TurnSpeed * Time.deltaTime);
+        transf.rotation = Quaternion.RotateTowards(transf.rotation, smoothRotation, turnSpeed * dt);
     }
+    
 }
